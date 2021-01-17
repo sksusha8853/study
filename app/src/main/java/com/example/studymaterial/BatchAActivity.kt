a9c4105adf5a26c040f308bfad9f18e8c83bc204
Amar-2003 <raghunathanmp69@gmail.com>
2021-01-17 16:15:07 +0530

    link update for cs103

--- app/src/main/java/com/example/studymaterial/BatchAActivity.kt
+++ app/src/main/java/com/example/studymaterial/BatchAActivity.kt
@@ -1,6 +1,7 @@
 package com.example.studymaterial
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,39 @@ class BatchAActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_batch_a)
+
+        val cs103Button: Button = findViewById(R.id.button9)
+        cs103Button.setOnClickListener{
+          val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1JUKW2XjKErWRV_ax3mgxwDUBS7l5TYS4"))
+            startActivity(intent)
+        }
+
+        val ph105Button: Button = findViewById(R.id.button10)
+        ph105Button.setOnClickListener{
+            val intent1 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ph105"))
+            startActivity(intent1)
+        }
+        val hs159Button: Button = findViewById(R.id.button11)
+        hs159Button.setOnClickListener{
+            val intent2 = Intent(Intent.ACTION_VIEW, Uri.parse("link for hs159"))
+            startActivity(intent2)
+        }
+        val ic153Button: Button = findViewById(R.id.button12)
+        ic153Button.setOnClickListener{
+            val intent3 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ic153"))
+            startActivity(intent3)
+        }
+        val ma105Button: Button = findViewById(R.id.button13)
+        ma105Button.setOnClickListener{
+            val intent4 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ma105"))
+            startActivity(intent4)
+        }
+        val ch103Button: Button = findViewById(R.id.button14)
+        ch103Button.setOnClickListener{
+            val intent5 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ch103"))
+            startActivity(intent5)
+        }
+
 
 
         }
--- app/src/main/java/com/example/studymaterial/BatchBActivity.kt
+++ app/src/main/java/com/example/studymaterial/BatchBActivity.kt
@@ -1,11 +1,46 @@
 package com.example.studymaterial
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BatchBActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_batch_b)
+
+        val ma105Button: Button = findViewById(R.id.button16)
+        ma105Button.setOnClickListener{
+            val intent6 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ma105"))
+            startActivity(intent6)
+        }
+        val ph106Button: Button = findViewById(R.id.button15)
+        ph106Button.setOnClickListener{
+            val intent7 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ph106"))
+            startActivity(intent7)
+        }
+        val ee104Button: Button = findViewById(R.id.button17)
+        ee104Button.setOnClickListener{
+            val intent8 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ma105"))
+            startActivity(intent8)
+        }
+        val hs108Button: Button = findViewById(R.id.button18)
+        hs108Button.setOnClickListener{
+            val intent9 = Intent(Intent.ACTION_VIEW, Uri.parse("link for hs108"))
+            startActivity(intent9)
+        }
+        val me106Button: Button = findViewById(R.id.button19)
+        me106Button.setOnClickListener{
+            val intent10 = Intent(Intent.ACTION_VIEW, Uri.parse("link for me105"))
+            startActivity(intent10)
+        }
+        val bse102Button: Button = findViewById(R.id.button20)
+        bse102Button.setOnClickListener{
+            val intent11 = Intent(Intent.ACTION_VIEW, Uri.parse("link for bse102"))
+            startActivity(intent11)
+        }
     }
+
 }
