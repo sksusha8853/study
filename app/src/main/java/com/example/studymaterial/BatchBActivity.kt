827737cd9ce6aed4a831af42f2acefa74dab35c2
Amar-2003 <raghunathanmp69@gmail.com>
2021-01-17 16:35:51 +0530

    link update for Batch B(Project over !!)

--- app/src/main/java/com/example/studymaterial/BatchBActivity.kt
+++ app/src/main/java/com/example/studymaterial/BatchBActivity.kt
@@ -13,22 +13,22 @@ class BatchBActivity : AppCompatActivity() {
 
         val ma105Button: Button = findViewById(R.id.button16)
         ma105Button.setOnClickListener{
-            val intent6 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ma105"))
+            val intent6 = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1XxoTTckJZyhiUZh-73pSKuvr-q17dJef?usp=sharing"))
             startActivity(intent6)
         }
         val ph106Button: Button = findViewById(R.id.button15)
         ph106Button.setOnClickListener{
-            val intent7 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ph106"))
+            val intent7 = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/16YHNLWxtxTQX6HZiOGu24aik1BbPiYz1?usp=sharing"))
             startActivity(intent7)
         }
         val ee104Button: Button = findViewById(R.id.button17)
         ee104Button.setOnClickListener{
-            val intent8 = Intent(Intent.ACTION_VIEW, Uri.parse("link for ma105"))
+            val intent8 = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1y1ZHXiVbFtVcfCtR9bb3FFOmeG87NphT?usp=sharing"))
             startActivity(intent8)
         }
         val hs108Button: Button = findViewById(R.id.button18)
         hs108Button.setOnClickListener{
-            val intent9 = Intent(Intent.ACTION_VIEW, Uri.parse("link for hs108"))
+            val intent9 = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/17phe--vX9LbLIFrqPPoYXdipAGEcJ4Hz?usp=sharing"))
             startActivity(intent9)
         }
         val me106Button: Button = findViewById(R.id.button19)
@@ -38,7 +38,7 @@ class BatchBActivity : AppCompatActivity() {
         }
         val bse102Button: Button = findViewById(R.id.button20)
         bse102Button.setOnClickListener{
-            val intent11 = Intent(Intent.ACTION_VIEW, Uri.parse("link for bse102"))
+            val intent11 = Intent(Intent.ACTION_VIEW, Uri.parse("https://drive.google.com/drive/folders/1sUYRk-tvkjyOtzHsB48ok3p96oWV0vaR?usp=sharing"))
             startActivity(intent11)
         }
     }
