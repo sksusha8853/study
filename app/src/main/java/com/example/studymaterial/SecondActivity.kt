64225f2ee2ee3d363743c35ed61f5e0fcb1da8a4
ash-100 <vashwinjino@gmal.com>
2021-01-17 13:05:49 +0530

    navigate

--- app/src/main/java/com/example/studymaterial/SecondActivity.kt
+++ app/src/main/java/com/example/studymaterial/SecondActivity.kt
@@ -1,11 +1,19 @@
 package com.example.studymaterial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val firstSemesterButton: Button = findViewById(R.id.button5)
+        firstSemesterButton.setOnClickListener {
+            val intent2 = Intent(this, ThirdActivity::class.java)
+            startActivity(intent2)
+        }
     }
 }
