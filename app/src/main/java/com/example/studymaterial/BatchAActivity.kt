851961bd16c10bf45cf59dd8dae534c0a85f1805
ash-100 <vashwinjino@gmal.com>
2021-01-17 13:30:48 +0530

    Added BatchAActivity and batchAButton

---
+++ app/src/main/java/com/example/studymaterial/BatchAActivity.kt
@@ -0,0 +1,15 @@
+package com.example.studymaterial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class BatchAActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batch_a)
+
+
+        }
+    }
--- app/src/main/java/com/example/studymaterial/ThirdActivity.kt
+++ app/src/main/java/com/example/studymaterial/ThirdActivity.kt
@@ -1,11 +1,19 @@
 package com.example.studymaterial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val batchAButton: Button = findViewById(R.id.button7)
+        batchAButton.setOnClickListener {
+            val intent3 = Intent(this, BatchAActivity::class.java)
+            startActivity(intent3)
+        }
     }
 }
